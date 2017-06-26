fe5c4308c2f6920829e3ce49ac0bf88c4ee079d4
Arun Kumar Yathiraj <Arun@Aruns-MacBook-Pro.local>
2017-06-26 11:52:29 +0530

    Frontend application code

---
+++ WebApp/src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './not-found.component';
+import { AppAuthGuard } from './app-auth-guard.service';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/dashboard/service',
+    pathMatch: 'full'
+  },
+  {
+    path: 'dashboard',
+    canLoad: [ AppAuthGuard ],
+    loadChildren: 'app/account/account.module#AccountModule'
+  },
+  {
+    path: 'admin-dashboard',
+    canLoad: [ AppAuthGuard ],
+    loadChildren: 'app/admin-account/admin-account.module#AdminAccountModule'
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+];
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule {}
